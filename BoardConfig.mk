026ed1e925f97b8c5b6027e4ebc45569193d7ea8
Quartz750 <145698054+Quartz750@users.noreply.github.com>
2026-07-19 18:03:05 +0800

    update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -236,7 +236,7 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libdebuggerd_client.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
-TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko rproc_qcom_common.ko q6_dlkm.ko qcom_q6v5.ko qcom_q6v5_pas.ko qcom_esoc.ko qcom_sysmon.ko goodix_core.ko goodix_fod.ko goodix_health.ko goodix_3626.ko focaltech_fts.ko focaltech_touch.ko fts_touch_spi.ko aw882xx_dlkm.ko leds-qpnp-vibrator-ldo.ko qcom-hv-haptics.ko qti_battery_charger.ko xiaomi_touch.ko hwmon.ko msm_video.ko mmi_annotate.ko mmi_info.ko mmi_charger.ko mmi_relay.ko sensors_class.ko awinic_sar.ko ilitek_v4_mmi.ko chipone_tddi_v3_mmi.ko leds_aw99703.ko"
+TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko leds-qpnp-vibrator-ldo.ko qcom-hv-haptics.ko qti_battery_charger.ko  hwmon.ko msm_video.ko mmi_annotate.ko mmi_info.ko mmi_charger.ko mmi_relay.ko sensors_class.ko awinic_sar.ko ilitek_v4_mmi.ko chipone_tddi_v3_mmi.ko leds_aw99703.ko"
 TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
 TW_HAS_USB_STORAGE := true
 TW_USB_STORAGE_PATH := /usb_otg
